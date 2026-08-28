f8212b17b821d8b4ba1bce086212db84a27460c1
Prakhar  Sharma <helofake98@gmail.com>
2026-08-28 13:57:24 +0530

    changes

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -27,12 +27,7 @@ const Stylesheet = () => (
   `}</style>
 );
 
-<<<<<<< HEAD
-// Glowing Logo Component
-// Web3GlowLogo component ko is se replace kar do
-=======
 
->>>>>>> e7168dcf605e9c92dcf4e8c62f09a9323eeeb87c
 const Web3GlowLogo = () => (
   <div
     style={{
